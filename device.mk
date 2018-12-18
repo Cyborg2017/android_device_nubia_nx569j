95d6c87d437ee15aa9a8ad77f1e95e20b372e175
mike dooley <mdooley@google.com>
2018-12-18 20:29:02 +0100

    nx569j: Enabling sound trigger V2_2
    V2_2 has the same functionality as V2_1 except it adds one new
    api, getModelState.
    
    Note: V2_1 will be removed in a subsequent cl.
    
    Test: built android and tested using SoundTriggerTestApp
    
    Bug: 70206501
    Change-Id: Id2647687732e79df252588d69f95d2a407e0092c
    Signed-off-by: AmolAmrit <amol.amrit03@outlook.com>
    (cherry picked from commit 3fcf7a7e093f1fdad8274a1620921d9dbeaee770)

--- device.mk
+++ device.mk
@@ -40,8 +40,8 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@5.0-impl \
     android.hardware.audio.effect@5.0-service \
-    android.hardware.soundtrigger@2.0-impl \
-    android.hardware.soundtrigger@2.0-service \
+    android.hardware.soundtrigger@2.2-impl \
+    android.hardware.soundtrigger@2.2-service \
     audio.a2dp.default \
     audio.r_submix.default \
     audio.usb.default \
