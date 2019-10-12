f62db93924b1d92c3f29437a5bbb6643d1c02e3b
AmolAmrit <amol.amrit03@outlook.com>
2019-10-12 19:10:33 +0530

    Revert "nx569j: Don't build XiaomiParts for now"
    This reverts commit 6d7e73c5450f0e23d840fdeef256ae7f10d83615.
    
    (cherry picked from commit 5290817ac9c23b9fa9484fe1b280ee35f9625a9c)

--- device.mk
+++ device.mk
@@ -134,8 +134,8 @@ PRODUCT_PACKAGES += \
     vendor.display.config@1.0_vendor
     
 # Doze mode
-PRODUCT_PACKAGES += \
-    NubiaDoze
+#PRODUCT_PACKAGES += \
+#    NubiaDoze
 
 # DRM
 PRODUCT_PACKAGES += \
