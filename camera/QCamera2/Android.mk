99cc6cb4402235ae1f3be7959cdae57056f46942
AmolAmrit <amol.amrit03@outlook.com>
2019-10-30 17:43:03 +0530

    nx569j: QCamera: Update path for HALs
    Signed-off-by: AmolAmrit <amol.amrit03@outlook.com>
    (cherry picked from commit e4fcc0356a18bead8d4ca3569b09199f7d75246d)

--- camera/QCamera2/Android.mk
+++ camera/QCamera2/Android.mk
@@ -79,8 +79,8 @@ LOCAL_C_INCLUDES := \
         $(LOCAL_PATH)/util \
         $(LOCAL_PATH)/HAL3 \
         hardware/libhardware/include/hardware \
-        $(call project-path-for,qcom-media)/libstagefrighthw \
-        $(call project-path-for,qcom-media)/mm-core/inc \
+        hardware/qcom-caf/msm8952/media/libstagefrighthw \
+        hardware/qcom-caf/msm8952/media/mm-core/inc \
         system/core/include/cutils \
         system/core/include/system \
         system/media/camera/include/system
@@ -109,7 +109,7 @@ endif
 LOCAL_C_INCLUDES += \
         $(TARGET_OUT_HEADERS)/qcom/display
 LOCAL_C_INCLUDES += \
-        $(call project-path-for,qcom-display)/libqservice
+        hardware/qcom-caf/msm8952/display/libqservice
 LOCAL_SHARED_LIBRARIES := liblog libhardware libutils libcutils libdl libsync
 LOCAL_SHARED_LIBRARIES += libmmcamera_interface libmmjpeg_interface libui libcamera_metadata
 LOCAL_SHARED_LIBRARIES += libqdMetaData libqservice libbinder
